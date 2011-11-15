cecd7f286c6683333b3b01d50ece3e5af9acf749
Mathieu Dumais-Savard <matdumsa@gmail.com>
2011-11-15 17:35:34 -0500

    - Added a console utility (re-using my P2's one)

---
+++ src/ca/concordia/comp479/application/Console.java
@@ -0,0 +1,10 @@
+package ca.concordia.comp479.application;
+
+import info.mathieusavard.application.console.InteractiveQuery;
+
+public class Console {
+
+	public static void main(String[] args) {
+		InteractiveQuery.main(args);
+	}
+}
