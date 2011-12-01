e01d28f032b792fa9f192b1e3dcf716b96e0f3b1
jerem6464 <jeremiemartinez@gmail.com>
2011-12-01 14:20:38 -0500

    Finally making commit work?

--- src/ca/concordia/comp479/application/Console.java
+++ src/ca/concordia/comp479/application/Console.java
@@ -5,6 +5,7 @@ import info.mathieusavard.application.console.ResultsPrinter;
 import info.mathieusavard.domain.WebDocument;
 import info.mathieusavard.domain.WeightedDocument;
 import info.mathieusavard.domain.corpus.Corpus;
+import info.mathieusavard.domain.queryprocessor.RankedResult;
 import info.mathieusavard.domain.queryprocessor.Result;
 
 public class Console {
@@ -19,9 +20,14 @@ public class Console {
 			@Override
 			public void printResult(Result r) {
 				WebDocument wd = (WebDocument) r.getResult();
-				System.out.print(r.getResult().getId() + " - " + r.getRank() + " - " + wd.getUrl());
-				System.out.println("\t" + r.getResult().getTitle());						
-				
+				if (r instanceof RankedResult){
+					RankedResult rR = (RankedResult)r;
+					System.out.print(rR.getResult().getId() + " - " + rR.getRank() + " - " + wd.getUrl());
+					System.out.println("\t" + rR.getResult().getTitle());
+				} else {
+					System.out.print(r.getResult().getId() + " - " + wd.getUrl());
+					System.out.println("\t" + r.getResult().getTitle());
+					}					
 			}
 			
 		});
